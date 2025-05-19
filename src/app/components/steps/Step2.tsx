cb7932eb59d740e37a2e4e33368f57e639b1f21c
Jose Manuel Gonzalez <33811865+Josestadele@users.noreply.github.com>
2025-05-19 06:18:43 -0400

    style- faltaba un stilo de de css

--- src/app/components/steps/Step2.tsx
+++ src/app/components/steps/Step2.tsx
@@ -90,6 +90,7 @@ export default function Step2() {
                 <input
                   type="text"
                   name="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                   value={travel.name}
                   onChange={(e) => handle(e, idx)}
                   required
@@ -100,6 +101,7 @@ export default function Step2() {
                 <input
                   type="date"
                   name="birthDate"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                   value={travel.birthDate}
                   onChange={(e) => handle(e, idx)}
                   required
@@ -109,6 +111,7 @@ export default function Step2() {
                 Document Type:
                 <select
                   name="documentType"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                   value={travel.documentType}
                   onChange={(e) => handle(e, idx)}
                 >
@@ -121,6 +124,7 @@ export default function Step2() {
                 <input
                   type="text"
                   name="documentNumber"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                   value={travel.documentNumber}
                   onChange={(e) => handle(e, idx)}
                   required
